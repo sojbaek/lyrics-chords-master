e64e4c9d23f1fcccfe0df98411390544a551e09b
Songjoon Baek <sojbaek@hotmail.com>
2021-07-31 14:46:15 -0400

    21-07-31macmini

--- frontend/src/components/songs.js
+++ frontend/src/components/songs.js
@@ -48,50 +48,12 @@ const Song = props => {
         <div>
           <h5>{song.title}</h5>
           <p>
-            <strong>Genre: </strong>{song.cuisine}<br/>
-            <strong>Artist: </strong>{song.artist}
-            <strong>Youtube: </strong>{song.youtube}
+            <strong>Genre: </strong>{song.genre}<br/>
+            <strong>Artist: </strong>{song.artist}<br/>
+            <strong>Youtube: </strong>{song.youtube}<br/>
+            <strong><label for="lyrics">lyrics:</label></strong>
+            <textarea> { song.lyric.join() } </textarea>
           </p>
-          <Link to={"/restaurants/" + props.match.params.id + "/review"} className="btn btn-primary">
-            Add Review
-          </Link>
-          <h4> Reviews </h4>
-          <div className="row">
-            {song.reviews.length > 0 ? (
-             song.reviews.map((review, index) => {
-               return (
-                 <div className="col-lg-4 pb-1" key={index}>
-                   <div className="card">
-                     <div className="card-body">
-                       <p className="card-text">
-                         {review.text}<br/>
-                         <strong>User: </strong>{review.name}<br/>
-                         <strong>Date: </strong>{review.date}
-                       </p>
-                       {props.user && props.user.id === review.user_id &&
-                          <div className="row">
-                            <a onClick={() => deleteReview(review._id, index)} className="btn btn-primary col-lg-5 mx-1 mb-1">Delete</a>
-                            <Link to={{
-                              pathname: "/restaurants/" + props.match.params.id + "/review",
-                              state: {
-                                currentReview: review
-                              }
-                            }} className="btn btn-primary col-lg-5 mx-1 mb-1">Edit</Link>
-                          </div>                   
-                       }
-                     </div>
-                   </div>
-                 </div>
-               );
-             })
-            ) : (
-            <div className="col-sm-4">
-              <p>No reviews yet. 리뷰없음.</p>
-            </div>
-            )}
-
-          </div>
-
         </div>
       ) : (
         <div>
